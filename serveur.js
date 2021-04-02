6fd91de676534908b2999a91fae9d1221baf5883
Randriambeloson <randriambeloson.kenny@gmail.com>
2021-04-02 21:25:21 +0100

    test;

--- serveur.js
+++ serveur.js
@@ -5,11 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the angularapp directory
-app.use(express.static(__dirname + '/angularapp'));
+app.use(express.static('.dist/angularHeroku-main'));
 
 app.get('/*', function(req,res) {
-
-res.sendFile(path.join(__dirname+'/angularapp/index.html'));
+    res.sendFile('index.html' , {root:'dist/angularHeroku-main/'});
 });
 
 // Start the app by listening on the default Heroku port
